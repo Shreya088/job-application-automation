4eff25759e4923dd78d1257cb4e4f94bb14a8164
Shreya088 <shreyabhardwaj0813@gmail.com>
2026-04-22 16:28:46 +0100

    job application automation repository initial vanilla version

---
+++ src/main/java/com/shreyaJavaProj/Application/Automation/ApplicationAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.shreyaJavaProj.Application.Automation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationAutomationApplication.class, args);
+	}
+
+}
